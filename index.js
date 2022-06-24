53f2ca1b0f3d31d25eacf510e4576f571ea992d7
Bastian1212 <bastianvillanueva@me.com>
2022-06-24 00:29:15 -0400

    clases ok

---
+++ index.js
@@ -0,0 +1,29 @@
+const indicadoresBuilder = require("./fabrica/indicadoresBuilder");
+
+
+
+const express = require('express')
+const app = express()
+
+
+let indicador1 = new indicadoresBuilder("C01").makeCalificacionCORFO("Crítico").makeNumeroIndicador("01").makeMisionUniversitaria("Segunda").makeNombre("prueba").makeTipoIndicador("Proceso").makeEje("Comercialización de Tecnología y Emprendimiento de Base Tecnológica").makeUnidad("-").makeFuenteInformacion("-").makeResponsable("-").makeFrecuencia("-").makePeticion("Aprobado").makeidMetrica(0).makeidMeta(0).build();
+
+
+
+
+
+
+
+app.set('port',3000)
+
+app.get('/', (req, res) =>{
+    res.send("prueba" + indicador1.getCalificacionCORFO())
+
+})
+
+
+app.listen(app.get('port'), ()=>{
+
+    console.log("3000 ")
+
+})
